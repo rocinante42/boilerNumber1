053d151df43fa60120ce90fb9bf5a756cafdbb79
rocinante42 <rovertoo@gmail.com>
2017-04-11 12:31:41 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+const jsonServer = require('json-server');
+
+const routes = require('./routes.json');
+
+const server = jsonServer.create(routes);
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.get('/echo', (req, res) => {
+  res.send('Estoy dentro de la api :)');
+});
+
+server.use('/api', router);
+server.listen(3001, () => {
+  console.log('JSON server is running');
+});
+
